af84bea3e4bf917bd4062ad1c1791973f2dfbc3c
francisakabo <francisakabo@gmail.com>
2020-04-09 14:17:06 +0000

    Added comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import post_list, post_detail, post_new, post_edit
+from . import views
 
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
-    path('post/<int:pk>/', post_detail, name='post_detail'),
-    path('post/new/', post_new, name='post_new'),
-    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
