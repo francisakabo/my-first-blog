d9785d2e2a9ae2ed5c3ffb4076378ebd404c9901
francisakabo <francisakabo@gmail.com>
2020-04-09 14:27:30 +0000

    Added Comment Model

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
 
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
-    
+
     def __str__(self):
         return self.title
 
